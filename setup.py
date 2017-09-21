540fccde0401f6ba1ffbe65c19c9e98c94ad0f5b
Will Barnes <wtb2@rice.edu>
2017-09-21 00:54:13 -0500

    more metadata

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ if not os.path.isdir(os.path.join(os.environ['HOME'], '.hissw')):
 
 setup(
     name='hissw',
-    version='1.0dev',
+    license='MIT',
+    version='0.1',
     author='Will Barnes',
     url='https://github.com/wtbarnes/hissw',
-    package_data={'hissw':['templates/*']},
-    packages=['hissw']
+    package_data={'hissw': ['templates/*']},
+    packages=['hissw'],
+    author_email='will.t.barnes@gmail.com',
+    description='SSWIDL code in your Python workflow'
 )
