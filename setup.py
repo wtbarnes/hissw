b7879a486cfb52364ee1f1d1624fc3f530e90a5c
Will Barnes <wtb2@rice.edu>
2018-09-12 01:36:54 -0500

    add more info

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
 import os
+import setuptools
 from distutils.core import setup
 
 # create home directory
 if not os.path.isdir(os.path.join(os.environ['HOME'], '.hissw')):
     os.mkdir(os.path.join(os.environ['HOME'], '.hissw'))
 
+with open('README.md', 'r') as f:
+    long_description=f.read()
+
 setup(
     name='hissw',
     license='MIT',
-    version='0.1',
+    version='1.0',
+    description='Seamlessly integrate SSWIDL code into your Python workflow',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Will Barnes',
+    author_email='will.t.barnes@gmail.com',
     url='https://github.com/wtbarnes/hissw',
     package_data={'hissw': ['templates/*']},
     packages=['hissw'],
-    author_email='will.t.barnes@gmail.com',
-    description='SSWIDL code in your Python workflow'
+    classifieers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intendend Audience :: Scientists',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='solar sun ssw solar-physics idl sswidl solarsoft',
+    project_urls={
+        'Documentation': 'https://wtbarnes.github.io/hissw/',
+        'Source': 'https://github.com/wtbarnes/hissw/',
+    },
+    install_requires=['jinja2', 'scipy'],
+    python_requires='>=3'
 )
